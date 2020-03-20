bc0291faf8f265e0353052333027fb46f67d1183
ulfahasanah <ulfa2912@outlook.com>
2020-03-20 10:34:46 +0700

    show winner name when there's a winner

--- src/views/Game.vue
+++ src/views/Game.vue
@@ -3,6 +3,7 @@
       <h1>JS Racer</h1>
       <p>{{ playerName }}</p>
       <p v-if="winner">we have a winner</p>
+      <p v-if="winnerName"> {{ winnerName }}</p>
       <div>
         <canvas
           ref="game"
@@ -25,43 +26,48 @@ export default {
   name: 'Game',
   data () {
     return {
-      id: "",
+      id: '',
       winner: false,
-      dice: 0
+      dice: 0,
+      winnerName: ''
     }
   },
   created () {
     this.socket = io('http://localhost:3000')
   },
   mounted () {
-     this.context = this.$refs.game.getContext('2d')
-    
-    this.socket.on('positions', data => {      
+    this.context = this.$refs.game.getContext('2d')
+
+    this.socket.on('positions', data => {
       this.$store.state.positions = data
 
       this.context.clearRect(0, 0, this.$refs.game.width, this.$refs.game.height)
       this.context.fillRect(this.$store.state.positions[0].x, this.$store.state.positions[0].y, 20, 20)
       this.context.fillRect(this.$store.state.positions[1].x, this.$store.state.positions[1].y, 20, 20)
     })
-    
+
     this.socket.on('winner', data => {
       if (data) {
         this.winner = true
+        this.winnerName = data
       }
     })
   },
   methods: {
     move () {
-       const payload = {
-        id: this.socket.id,
-        dice: this.dice + 20
+      if (!this.winner) {
+        const payload = {
+          id: this.socket.id,
+          dice: this.dice + 20,
+          name: this.playerName
+        }
+        this.socket.emit('move', payload)
+        this.dice = 0
       }
-      this.socket.emit('move', payload)
-      this.dice = 0
     }
   },
   computed: {
-    ...mapState(['playerName']),
+    ...mapState(['playerName'])
   }
 }
 </script>
