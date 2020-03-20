3bc7f4b0ecb622f0a13dbdf1ad5fbdc85add6d1a
MickeyStrike <irfanmaulana281299@gmail.com>
2020-03-20 10:40:42 +0700

    add new fitur

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,6 @@
 <template>
   <div id="app">
     <Game />
-    <div class="container" style="margin:0;padding:0">
-      <div class="row">
-        <div class="col-sm-8">
-          <div class="bg">
-            <img class="mobil" src="./assets/car.png" />
-            <img class="mobil2" src="./assets/car.png" />
-          </div>
-        </div>
-        <div class="col-sm-4">
-          <div class="w3-container ml-5">
-            <h2>Rank</h2>
-            <ul class="w3-ul w3-card-4">
-              <li class="w3-bar">
-                <img src="https://www.w3schools.com/w3css/img_avatar2.png" class="w3-bar-item w3-circle w3-hide-small" style="width:85px">
-                <div class="w3-bar-item mt-2">
-                  <span>Player1</span>
-                </div>
-              </li>
-              <li class="w3-bar">
-                <img src="https://www.w3schools.com/w3css/img_avatar5.png" class="w3-bar-item w3-circle w3-hide-small" style="width:85px">
-                <div class="w3-bar-item mt-2">
-                  <span>Player2</span>
-                </div>
-              </li>
-            </ul>
-          </div>
-          <br/>
-          <button class="btn btn-primary ml-5">MOVE</button>
-        </div>
-      </div>
-    </div>
   </div>
 </template>
 
@@ -47,69 +16,4 @@ export default {
 </script>
 
 <style>
-.bg {
-  background: url("https://ak0.picdn.net/shutterstock/videos/1028600570/thumb/1.jpg");
-  background-position: 50%;
-  height: 100vh;
-}
-.mobil {
-  top: 80%; /*tinggal mainan top*/
-  left: 27%;
-  height: 120px;
-  width: 80px;
-  z-index: 0;
-  position: absolute;
-}
-.mobil2 {
-  top: 80%; /*tinggal mainan top*/
-  left: 62%;
-  height: 120px;
-  width: 80px;
-  z-index: 0;
-  position: absolute;
-}
-.w3 {
-  width: 400px;
-}
-.w3-container, .w3-panel {
-  padding: 0.01em 16px;
-}
-.w3-card-4, .w3-hover-shadow:hover{
-  box-shadow: 0 4px 10px 0 rgba(0,0,0,0.2),0 4px 20px 0 rgba(0,0,0,0.19);
-  width: 600px;
-}
-.w3-ul {
-  list-style-type: none;
-  padding:0;
-  margin: 0;
-}
-.w3-ul li {
-  padding:8px 16px;
-  border-bottom:1px solid #ddd;
-}
-.w3-bar {
-  width: 100%;
-  overflow: hidden;
-}
-.w3-bar .w3-button {
-  padding: 8px 16px;
-  width: auto;
-  border: auto;
-  display: block;
-  outline:0;
-}
-.w3-white, .w3-hover-white:hover {
-  color: #000 !important;
-  background-color: #fff !important;
-}
-.w3-circle {
-  height: 60px;
-}
-.w3-bar .w3-bar-item {
-  padding: 8px 16px;
-  float: left;
-  border: none;
-  display: block;
-  outline: 0;
-}
 </style>
--- src/components/Game.vue
+++ src/components/Game.vue
@@ -1,6 +1,36 @@
 <template>
   <div>
-      <h1>JS Racer</h1>
+    <div class="container" style="margin:0;padding:0">
+      <div class="row">
+        <div class="col-sm-8">
+          <div class="bg">
+            <img class="mobil" src=".././assets/car.png" />
+            <img class="mobil2" src=".././assets/car.png" />
+            <button class="btn btn-danger console">MOVE</button>
+          </div>
+        </div>
+        <div class="col-sm-4">
+          <div class="w3-container ml-5">
+            <h2>Rank</h2>
+            <ul class="w3-ul w3-card-4">
+              <li class="w3-bar w3-bar-1">
+                <img src="https://www.w3schools.com/w3css/img_avatar2.png" class="w3-bar-item w3-circle w3-hide-small" style="width:85px">
+                <div class="w3-bar-item mt-2">
+                  <span>Player1</span>
+                </div>
+              </li>
+              <li class="w3-bar w3-bar-2">
+                <img src="https://www.w3schools.com/w3css/img_avatar5.png" class="w3-bar-item w3-circle w3-hide-small" style="width:85px">
+                <div class="w3-bar-item mt-2">
+                  <span>Player2</span>
+                </div>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+      <!-- <h1>JS Racer</h1>
       <p v-if="winner">we have a winner</p>
       <div>
         <canvas
@@ -12,7 +42,7 @@
         <p>
           <button @click="move()">GO!</button>
         </p>
-      </div>
+      </div> -->
   </div>
 </template>
 
@@ -78,4 +108,112 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+.w3-bar-1 {
+  position: relative;
+  animation: myfirst 1s 1;
+  animation-direction: normal;
+  top: 77px;
+}
+/* .w3-bar-1{
+  -webkit-transition: 3s;
+  -moz-transition: 3s;
+  -ms-transition: 3s;
+  -o-transition: 3s;
+  transition: 3s;
+  top: 77px;
+} */
+@keyframes myfirst {
+  0%   {background: red; left: 0px; top: 0px;}
+  100%  {background: green; left: 0px; top: 77px;}
+}
+.w3-bar-2 {
+  position: relative;
+  animation: myfirst2 1s 1;
+  animation-direction: normal;
+  top:-77px
+}
+@keyframes myfirst2 {
+  0%   {background: red; left: 0px; top: 0px;}
+  100%  {background: green; left: 0px; top: -77px;}
+}
+/* .w3-bar-2{
+  -webkit-transition: 3s;
+  -moz-transition: 3s;
+  -ms-transition: 3s;
+  -o-transition: 3s;
+  transition: 3s;
+  top: -77px;
+} */
+.console {
+  border-radius: 50%;
+  position: absolute;
+  top: 82%;
+  left: 45%;
+  height: 75px;
+}
+.bg {
+  background: url("https://ak0.picdn.net/shutterstock/videos/1028600570/thumb/1.jpg");
+  background-position: 50%;
+  height: 100vh;
+}
+.mobil {
+  top: 80%; /*tinggal mainan top*/
+  left: 27%;
+  height: 120px;
+  width: 80px;
+  z-index: 0;
+  position: absolute;
+}
+.mobil2 {
+  top: 80%; /*tinggal mainan top*/
+  left: 62%;
+  height: 120px;
+  width: 80px;
+  z-index: 0;
+  position: absolute;
+}
+.w3 {
+  width: 400px;
+}
+.w3-container, .w3-panel {
+  padding: 0.01em 16px;
+}
+.w3-card-4, .w3-hover-shadow:hover{
+  box-shadow: 0 4px 10px 0 rgba(0,0,0,0.2),0 4px 20px 0 rgba(0,0,0,0.19);
+  width: 40vw;
+}
+.w3-ul {
+  list-style-type: none;
+  padding:0;
+  margin: 0;
+}
+.w3-ul li {
+  padding:8px 16px;
+  border-bottom:1px solid #ddd;
+}
+.w3-bar {
+  width: 100%;
+  overflow: hidden;
+}
+.w3-bar .w3-button {
+  padding: 8px 16px;
+  width: auto;
+  border: auto;
+  display: block;
+  outline:0;
+}
+.w3-white, .w3-hover-white:hover {
+  color: #000 !important;
+  background-color: #fff !important;
+}
+.w3-circle {
+  height: 60px;
+}
+.w3-bar .w3-bar-item {
+  padding: 8px 16px;
+  float: left;
+  border: none;
+  display: block;
+  outline: 0;
+}
 </style>
